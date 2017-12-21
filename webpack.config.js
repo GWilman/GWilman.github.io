3383929ecd28925fcc5b2e2bdce2667147ad63ac
GWilman <georgewilman1@gmail.com>
2017-12-21 11:08:30 +0000

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,32 @@
-const path = require('path');
 const webpack = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpack = new HtmlWebpackPlugin({
-  template: 'src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
-
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CopyWebpack = new CopyWebpackPlugin([
-  { from: './src/assets', to: 'assets' }
-]);
-
-const HotModuleReplcement = new webpack.HotModuleReplacementPlugin();
-
 module.exports = {
-  entry: './src/app.js',
+  context: __dirname,
+  entry: './index.js',
   output: {
-    path: path.resolve('public'),
-    filename: 'app.js',
-    publicPath: '/'
+    path: __dirname + '/__build__',
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.scss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
-      { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
     ]
   },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    port: 8000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        secure: false
-      }
-    }
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
-  plugins: [HotModuleReplcement, HtmlWebpack, CopyWebpack]
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
 };
