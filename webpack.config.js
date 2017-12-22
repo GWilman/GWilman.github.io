2f9857cfe4b661ec45c1916677245fc01c2f8bf9
GWilman <georgewilman1@gmail.com>
2017-12-22 15:48:00 +0000

    mvp site complete

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: ['style-loader', 'css-loader'] }
+      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
+      { test: /\.scss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
     ]
   },
 
